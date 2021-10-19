ba26f6b3f4f81e01b960708b6b14dcd6b270b6d3
Jannik Weisser <jannik.weisser@hs-furtwangen.de>
2021-10-19 10:57:48 +0200

    A1 Abgabe Verbesserungen
    Video wird nicht in schleife erzeugt, direkt mit Bildern (kein Array), Systemvorraussetzungen genannt

--- assignments/01/Abgabe1_Optical_Illusion.py
+++ assignments/01/Abgabe1_Optical_Illusion.py
@@ -6,87 +6,81 @@ import os
 
 # task https://github.com/uhahne/gdv-WiSe2021_22/blob/main/assignments/Assignement01_OpticalIllusion.md
 
-#Define variables
+# Define variables
 width = 400
 height = 300
 partHeight = 40
 partWidth = 40
 border = 50
 
-#Create a picture of a gradient with an array. That array generates pixels from 0(white) to 1(black), width indicates how many pixels need to get generated
+# Create a picture of a gradient with an array. That array generates pixels from 0(white) to 1(black), width indicates how many pixels need to get generated
 img_org = np.tile(np.linspace(1, 0, width), (height, 1))
 
-#Create picture parts by making a copy of the original picture
+# Create picture parts by making a copy of the original picture
 partImg = img_org[math.ceil(height/2-partHeight/2):math.ceil(height/2+partHeight/2),
                   math.ceil(width/2-partWidth/2):math.ceil(width/2+partWidth/2)]
 
 
-#Name and create window
-title = "Illusion"  #Title is shown
+# Name and create window
+title = "Illusion"  # Title is shown
 cv2.namedWindow(title, cv2.WINDOW_GUI_NORMAL)
 
-#Define variables for loop
+# Define variables for loop
 position = border
 runInRightDirection = True
 firstRunFLeftToRightABack = True
-imgArray = []
+
+# Definde variables for video
+fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+fps = 30
+foldername = 'output'
+# create outputfolder for video if doesnt already exist
+if not os.path.exists(foldername):
+    os.makedirs(foldername)
+video_filename = foldername + '/A1_Optical_Illusion_Video.avi'
+video = cv2.VideoWriter(video_filename, fourcc, fps, (width, height), False)
+size = (img_org.shape[1], img_org.shape[0])
 
 while True:
-    img = copy.copy(img_org)  #Making a copy of the original picture
+    img = copy.copy(img_org)  # Making a copy of the original picture
 
-    #Laying the moving picture part on the background (original picture)
+    # Laying the moving picture part on the background (original picture)
     img[math.ceil(height/2-partHeight/2):math.ceil(height/2 +
                                                    partHeight/2), position:partWidth+position] = partImg
 
-    if firstRunFLeftToRightABack: #Code gets changed for the first run, just important for the creation of the video
-        #Copy of the original picture gets append to imgArray
-        imgArray.append(img)
-        #Size of the picture gets saved 
-        size = (img.shape[1], img.shape[0])
+    # Static picture parts get laid on top of the background
+    img[border:border+partHeight, width-border-partWidth:width-border] = partImg
+    img[border:border+partHeight, border:border+partWidth] = partImg
 
-    #The part of the picture that’s supposed to move, moves one position to the right
+    if firstRunFLeftToRightABack:  # Code gets changed for the first run, just important for the creation of the video
+        video.write(np.uint8(255 * img))  # Adds Images to Video
+
+    # The part of the picture that’s supposed to move, moves one position to the right
     if runInRightDirection:
         position = position+1
 
-   #The part of the picture that’s supposed to move, moves one position to the left
+   # The part of the picture that’s supposed to move, moves one position to the left
     else:
         position = position-1
 
-    #If the moving picture hits the border on the right side, the variable changes so the part moves to the left
+    # If the moving picture hits the border on the right side, the variable changes so the part moves to the left
     if position == width-partWidth-border:
         runInRightDirection = False
 
-    #If the moving picture hits the border on the left side, the variable changes so the part moves to the right
+    # If the moving picture hits the border on the left side, the variable changes so the part moves to the right
     if position == border:
         runInRightDirection = True
 
-        #At the end of the first run, a video gets created from the picture array
+        # At the end of the first run, a video gets created from the picture array
         if firstRunFLeftToRightABack:
-            fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-            fps = 30
-            foldername = 'output'
-            if not os.path.exists(foldername):
-                os.makedirs(foldername)
-            video_filename = foldername + '/A1_Optical_Illusion_Video.avi'
-            video = cv2.VideoWriter(
-                video_filename, fourcc, fps, (width, height), False)
-
-            #Each picture in the array gets append to each other to create a video
-            for i in range(len(imgArray)):
-                video.write(np.uint8(255 * imgArray[i]))
-
-            #Video file gets released
+            # Video file gets released
             video.release()
             firstRunFLeftToRightABack = False
             print("Video done")
 
-    #Static picture parts get laid on top of the background 
-    img[border:border+partHeight, width-border-partWidth:width-border] = partImg
-    img[border:border+partHeight, border:border+partWidth] = partImg
-
-     #Animation is shown
+    #Animation is shown
     cv2.imshow(title, img)
-    if cv2.waitKey(5) == ord("q"):  #Programm gets closed
+    if cv2.waitKey(5) == ord("q"):  # Programm gets closed
         break
 
 cv2.destroyAllWindows()
--- assignments/01/Readme.md
+++ assignments/01/Readme.md
@@ -1,6 +1,9 @@
 # Gruppe 1 - Lara Franke und Jannik Weisser - Optical Illusion
 ## Kurze Erklärung
 Optische Illusion, bei welcher ein Gradientenbild erzeugt wird und ein Ausschnitt aus der Mitte nach links und rechts bewegt wird und scheinbar seine Farbe ändert. In Wirklichkeit ändert sich die Farbe jedoch nicht!
+
+## Benötigte Ressourcen
+Es wurde Python 3.9.7 verwendet mit den Modulen (über pip nachinstallieren) OpenCV (Version 4.5.3) und Numpy. Diese werden beim Start importiert.
 ## Code Erklärung
 Nach dem import werden verschiedene Variablen wie die Bildgröße und die Größe des Ausschnitts, welcher bewegt werden soll, festgelegt. Diese können auch geändert werden. Allerdings findet keine Überprüfung der Werte statt, es sollten also sinnvolle Werte gewählt werden.
 
