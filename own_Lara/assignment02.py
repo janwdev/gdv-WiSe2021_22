925f07de5556baf11ba3ff9997ef16ba22b94c05
larafra99 <lara.sofie.franke@hs-furtwangen.de>
2021-11-08 15:10:18 +0100

    Unnoetiges geloescht

--- own_Lara/assignment02.py
+++ own_Lara/assignment02.py
@@ -8,7 +8,6 @@ Sources: <Sources of inspiration and collaboration (persons, videos, web pages, 
 
 import cv2
 import glob  # for loading all images from a directory
-import numpy as np
 
 # Goal: Count the number of all colored balls in the images
 
@@ -24,10 +23,10 @@ gt_list = (num_red, num_green, num_blue, num_yellow, num_white, num_pink)
 # define color ranges in HSV, note that OpenCV uses the following ranges H: 0-179, S: 0-255, V: 0-255
 
 hue_range = 5
-saturation_range =50
+saturation_range = 50
 value_range = 50
+
 # Values are RGB
-#130-179,145-200,100-200
 hue_red = 179
 saturation_red = 168
 value_red = 173
@@ -36,13 +35,6 @@ lower_bound_red = (hue_red-hue_range, saturation_red -
 upper_bound_red = (hue_red+hue_range, saturation_red +
                    saturation_range, value_red+value_range)
 
-# color = np.uint8([[[61,59,173]]]) # BGR
-# hsv_color = cv2.cvtColor(color,cv2.COLOR_BGR2HSV)
-# print("red: ",hsv_color)
-
-hue_range = 5
-saturation_range =50
-value_range = 50
 hue_green = 46
 saturation_green = 181
 value_green = 168
@@ -51,9 +43,6 @@ lower_bound_green = (hue_green-hue_range, saturation_green -
 upper_bound_green = (hue_green+hue_range, saturation_green +
                      saturation_range, value_green+value_range)
 
-# hue_range = 5
-# saturation_range =50
-# value_range = 50
 hue_blue = 99
 saturation_blue = 233
 value_blue = 171
@@ -62,9 +51,6 @@ lower_bound_blue = (hue_blue-hue_range, saturation_blue -
 upper_bound_blue = (hue_blue+hue_range, saturation_blue +
                     saturation_range, value_blue+value_range)
 
-# hue_range = 5
-# saturation_range =50
-# value_range = 50
 hue_yellow = 28
 saturation_yellow = 207
 value_yellow = 255
@@ -73,9 +59,6 @@ lower_bound_yellow = (hue_yellow-hue_range, saturation_yellow -
 upper_bound_yellow = (hue_yellow+hue_range, saturation_yellow +
                       saturation_range, value_yellow+value_range)
 
-# hue_range = 5
-# saturation_range =50
-# value_range = 50
 hue_white = 29
 saturation_white = 31
 value_white = 255
@@ -84,9 +67,6 @@ lower_bound_white = (hue_white-hue_range, saturation_white -
 upper_bound_white = (hue_white+hue_range, saturation_white +
                      saturation_range, value_white+value_range)
 
-# hue_range = 5
-# saturation_range =50
-# value_range = 50
 hue_pink = 7
 saturation_pink = 110
 value_pink = 250
@@ -94,6 +74,13 @@ lower_bound_pink = (hue_pink-hue_range, saturation_pink -
                     saturation_range, value_pink-value_range)
 upper_bound_pink = (hue_pink+hue_range, saturation_pink +
                     saturation_range, value_pink+value_range)
+
+
+color_lower_bound = [lower_bound_red, lower_bound_green, lower_bound_blue,
+                     lower_bound_yellow, lower_bound_white, lower_bound_pink]
+color_upper_bound = [upper_bound_red, upper_bound_green, upper_bound_blue,
+                     upper_bound_yellow, upper_bound_white, upper_bound_pink]
+
 
 def morph_shape(val):
     if val == 0:
@@ -128,95 +115,67 @@ def closing(img, size, shape):
     return cv2.morphologyEx(img, cv2.MORPH_CLOSE, element)
 
 
-# set color under test
 num_colors = 6
 color_names = ['red', 'green', 'blue', 'yellow', 'white', 'pink']
 
-
-num_labels = 0  # TODO: implement something to set this variable
+num_labels = 0
 num_rejected = 1
-
-# setting the parameters that work for all colors
-color_lower_bound = [lower_bound_red, lower_bound_green, lower_bound_blue, lower_bound_yellow, lower_bound_white, lower_bound_pink]
-color_upper_bound = [upper_bound_red, upper_bound_green, upper_bound_blue, upper_bound_yellow, upper_bound_white, upper_bound_pink]
-# set individual (per color) parameters
-
 
 kernel_size = 3
 kernel_shape = morph_shape(2)
 connectivity = 8
 
-# circle_size = 10
-# circle_thickness = 5
 min_size = 10
 
-def ownAlgorithm(img, height, width, c):
+
+def color_counter(img, height, width, c):
     global num_rejected
     global num_labels
     num_rejected = 1
-    print(color_names[c])
     hsv_img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
     mask = cv2.inRange(hsv_img, color_lower_bound[c], color_upper_bound[c])
     cv2.imshow('Original image', img)
     result = cv2.bitwise_and(img, img, mask=mask)
-    cv2.imshow('Masked image',result)
+    cv2.imshow('Masked image', result)
 
-    # mask = opening(mask,kernel_size, kernel_shape)
-    # mask = closing(mask,kernel_size, kernel_shape)
-
-    if c == 0:
-        mask = opening(mask,2, kernel_shape)
-        mask = dilatation(mask, 4, kernel_shape)
-        mask = closing(mask,kernel_size, kernel_shape)
-    if c== 3:
-        mask = opening(mask,2, kernel_shape)
-        # mask = dilatation(mask, kernel_size, kernel_shape)
-    else:
-        mask = opening(mask,kernel_size, kernel_shape)
+    if c == 0:  # red
+        mask = opening(mask, kernel_size-1, kernel_shape)
+        mask = dilatation(mask, kernel_size+1, kernel_shape)
         mask = closing(mask, kernel_size, kernel_shape)
-    
-    (numLabels, labels, stats, centroids) = cv2.connectedComponentsWithStats(mask,connectivity,cv2.CV_32S)
-    num_labels = numLabels
-    for i in range(1,numLabels):
+    if c == 3:  # yellow
+        mask = opening(mask, kernel_size-1, kernel_shape)
+    else:
+        mask = opening(mask, kernel_size, kernel_shape)
+        mask = closing(mask, kernel_size, kernel_shape)
+
+    (num_labels, labels, stats, centroids) = cv2.connectedComponentsWithStats(
+        mask, connectivity, cv2.CV_32S)
+    for i in range(1, num_labels):
         x = stats[i, cv2.CC_STAT_LEFT]
         y = stats[i, cv2.CC_STAT_TOP]
         w = stats[i, cv2.CC_STAT_WIDTH]
         h = stats[i, cv2.CC_STAT_HEIGHT]
         if w < min_size or h < min_size:
-            print ('Found a too small component.')
+            print('Found a too small component.')
             num_rejected += 1
-            continue # found component is too small to be correct 
-        # #Rundheitspruefung
-        # if w > h:
-        #     roundness = 1.0 / (w/h)
-        # elif h > w:
-        #     roundness = 1.0 / (h/w)  
-        # if (roundness < .9):
-        #     print ('Found a component that is not round enough.')
-        #     num_rejected += 1
-        #     continue # ratio of width and height is not suitable
+            continue  # found component is too small to be correct
 
-    print("labels ", numLabels-1)
-    print("rejected ", num_rejected-1)
     result = cv2.bitwise_and(img, img, mask=mask)
-    cv2.imshow('Masked image later',result)
+    cv2.imshow('Masked image later', result)
+
 
 num_test_images_succeeded = 0
 for img_name in glob.glob('images/chewing_gum_balls*.jpg'):
     # load image
     print('Searching for colored balls in image:', img_name)
-
     all_colors_correct = True
-
     for c in range(0, num_colors):
-
         img = cv2.imread(img_name, cv2.IMREAD_COLOR)
         height = img.shape[0]
         width = img.shape[1]
 
-        # TODO: Insert your algorithm here
-        ownAlgorithm(img, height, width, c)
-        
+        color_counter(img, height, width, c)
+
         num_final_labels = num_labels-num_rejected
 
         success = (num_final_labels == int(gt_list[c]))
@@ -238,9 +197,6 @@ for img_name in glob.glob('images/chewing_gum_balls*.jpg'):
         if ((img_name == 'images\chewing_gum_balls01.jpg')
             or (img_name == 'images\chewing_gum_balls04.jpg')
                 or (img_name == 'images\chewing_gum_balls06.jpg')):
-            # show the original image with drawings in one window
-            # cv2.imshow('Original image', img)
-            # show other images?
 
             cv2.waitKey(0)
             cv2.destroyAllWindows()
