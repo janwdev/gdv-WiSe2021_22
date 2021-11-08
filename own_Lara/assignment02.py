d2aa93f9da604c7eeeaf2e9a987aef4bc97fad49
larafra99 <lara.sofie.franke@hs-furtwangen.de>
2021-11-08 14:53:31 +0100

    Abgabe 2 funktioniert :D

--- own_Lara/assignment02.py
+++ own_Lara/assignment02.py
@@ -22,20 +22,12 @@ num_red = 6
 gt_list = (num_red, num_green, num_blue, num_yellow, num_white, num_pink)
 
 # define color ranges in HSV, note that OpenCV uses the following ranges H: 0-179, S: 0-255, V: 0-255
-# hue_range = 10
-# saturation_range = 100
-# value_range = 100
+
 hue_range = 5
 saturation_range =50
 value_range = 50
-# rot in bild 3 wird zu klein, bei allen anderen wachsen die Kugeln zusammen 
 # Values are RGB
-
-# red
-# hue_red = 359
-# saturation_red = 66
-# value_red = 68
-
+#130-179,145-200,100-200
 hue_red = 179
 saturation_red = 168
 value_red = 173
@@ -43,14 +35,14 @@ lower_bound_red = (hue_red-hue_range, saturation_red -
                    saturation_range, value_red-value_range)
 upper_bound_red = (hue_red+hue_range, saturation_red +
                    saturation_range, value_red+value_range)
+
 # color = np.uint8([[[61,59,173]]]) # BGR
 # hsv_color = cv2.cvtColor(color,cv2.COLOR_BGR2HSV)
 # print("red: ",hsv_color)
 
-# green
-# hue_green = 92
-# saturation_green = 71
-# value_green = 66
+hue_range = 5
+saturation_range =50
+value_range = 50
 hue_green = 46
 saturation_green = 181
 value_green = 168
@@ -58,14 +50,10 @@ lower_bound_green = (hue_green-hue_range, saturation_green -
                      saturation_range, value_green-value_range)
 upper_bound_green = (hue_green+hue_range, saturation_green +
                      saturation_range, value_green+value_range)
-# color = np.uint8([[[49,168,105]]]) # BGR
-# hsv_color = cv2.cvtColor(color,cv2.COLOR_BGR2HSV)
-# print("green: ",hsv_color)
 
-# blue
-# hue_blue = 198
-# saturation_blue = 91
-# value_blue = 67
+# hue_range = 5
+# saturation_range =50
+# value_range = 50
 hue_blue = 99
 saturation_blue = 233
 value_blue = 171
@@ -73,14 +61,10 @@ lower_bound_blue = (hue_blue-hue_range, saturation_blue -
                     saturation_range, value_blue-value_range)
 upper_bound_blue = (hue_blue+hue_range, saturation_blue +
                     saturation_range, value_blue+value_range)
-# color = np.uint8([[[171,124,15]]]) # BGR
-# hsv_color = cv2.cvtColor(color,cv2.COLOR_BGR2HSV)
-# print("blue: ",hsv_color)
 
-# yellow
-# hue_yellow = 55
-# saturation_yellow = 81
-# value_yellow = 100
+# hue_range = 5
+# saturation_range =50
+# value_range = 50
 hue_yellow = 28
 saturation_yellow = 207
 value_yellow = 255
@@ -88,14 +72,10 @@ lower_bound_yellow = (hue_yellow-hue_range, saturation_yellow -
                       saturation_range, value_yellow-value_range)
 upper_bound_yellow = (hue_yellow+hue_range, saturation_yellow +
                       saturation_range, value_yellow+value_range)
-# color = np.uint8([[[48,238,255]]]) # BGR
-# hsv_color = cv2.cvtColor(color,cv2.COLOR_BGR2HSV)
-# print("yellow: ",hsv_color)
 
-# white
-# hue_white = 59
-# saturation_white = 12
-# value_white = 100
+# hue_range = 5
+# saturation_range =50
+# value_range = 50
 hue_white = 29
 saturation_white = 31
 value_white = 255
@@ -103,14 +83,10 @@ lower_bound_white = (hue_white-hue_range, saturation_white -
                      saturation_range, value_white-value_range)
 upper_bound_white = (hue_white+hue_range, saturation_white +
                      saturation_range, value_white+value_range)
-# color = np.uint8([[[224,254,255]]]) # BGR
-# hsv_color = cv2.cvtColor(color,cv2.COLOR_BGR2HSV)
-# print("white: ",hsv_color)
 
-# pink
-# hue_pink = 14
-# saturation_pink = 43
-# value_pink = 98
+# hue_range = 5
+# saturation_range =50
+# value_range = 50
 hue_pink = 7
 saturation_pink = 110
 value_pink = 250
@@ -118,12 +94,7 @@ lower_bound_pink = (hue_pink-hue_range, saturation_pink -
                     saturation_range, value_pink-value_range)
 upper_bound_pink = (hue_pink+hue_range, saturation_pink +
                     saturation_range, value_pink+value_range)
-# color = np.uint8([[[142,168,250]]]) # BGR
-# hsv_color = cv2.cvtColor(color,cv2.COLOR_BGR2HSV)
-# print("pink: ",hsv_color)
 
-# morphological operations
-# optional mapping of values with morphological shapes
 def morph_shape(val):
     if val == 0:
         return cv2.MORPH_RECT
@@ -169,20 +140,14 @@ num_rejected = 1
 color_lower_bound = [lower_bound_red, lower_bound_green, lower_bound_blue, lower_bound_yellow, lower_bound_white, lower_bound_pink]
 color_upper_bound = [upper_bound_red, upper_bound_green, upper_bound_blue, upper_bound_yellow, upper_bound_white, upper_bound_pink]
 # set individual (per color) parameters
-# hsv_red = (hue_red, saturation_red, value_red)
-# hsv_blue = (hue_blue, saturation_blue, value_blue)
-# hsv_pink = (hue_pink, saturation_pink, value_pink)
-# hsv_white = (hue_white, saturation_white, value_white)
-# hsv_green = (hue_green, saturation_green, value_green)
-# hsv_yellow = (hue_yellow, saturation_yellow, value_yellow)
-# hsv = [hue_red, hsv_green, hsv_blue, hsv_yellow, hsv_white, hsv_pink]
+
 
 kernel_size = 3
 kernel_shape = morph_shape(2)
 connectivity = 8
 
-circle_size = 10
-circle_thickness = 5
+# circle_size = 10
+# circle_thickness = 5
 min_size = 10
 
 def ownAlgorithm(img, height, width, c):
@@ -196,12 +161,19 @@ def ownAlgorithm(img, height, width, c):
     result = cv2.bitwise_and(img, img, mask=mask)
     cv2.imshow('Masked image',result)
 
-    #mask = opening(mask,kernel_size, kernel_shape)
-    #mask = closing(mask,kernel_size, kernel_shape)
+    # mask = opening(mask,kernel_size, kernel_shape)
+    # mask = closing(mask,kernel_size, kernel_shape)
 
-    #mask = erosion(mask,kernel_size, kernel_shape)
-    #mask = dilatation(mask,kernel_size, kernel_shape)
-    #mask = closing(mask,kernel_size, kernel_shape)
+    if c == 0:
+        mask = opening(mask,2, kernel_shape)
+        mask = dilatation(mask, 4, kernel_shape)
+        mask = closing(mask,kernel_size, kernel_shape)
+    if c== 3:
+        mask = opening(mask,2, kernel_shape)
+        # mask = dilatation(mask, kernel_size, kernel_shape)
+    else:
+        mask = opening(mask,kernel_size, kernel_shape)
+        mask = closing(mask, kernel_size, kernel_shape)
     
     (numLabels, labels, stats, centroids) = cv2.connectedComponentsWithStats(mask,connectivity,cv2.CV_32S)
     num_labels = numLabels
